7b87a8dd8e30f2977e4c39d6dc1d7500e6da26ac
hheiling <hheiling@live.unc.edu>
2019-11-04 15:35:05 -0500

    Update to Namespace and Description files (initial devtools::document)

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,18 +2,18 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 rcpparma_hello_world <- function() {
-    .Call(`_IsoDeconvMM_rcpparma_hello_world`)
+    .Call('_IsoDeconvMM_rcpparma_hello_world', PACKAGE = 'IsoDeconvMM')
 }
 
 rcpparma_outerproduct <- function(x) {
-    .Call(`_IsoDeconvMM_rcpparma_outerproduct`, x)
+    .Call('_IsoDeconvMM_rcpparma_outerproduct', PACKAGE = 'IsoDeconvMM', x)
 }
 
 rcpparma_innerproduct <- function(x) {
-    .Call(`_IsoDeconvMM_rcpparma_innerproduct`, x)
+    .Call('_IsoDeconvMM_rcpparma_innerproduct', PACKAGE = 'IsoDeconvMM', x)
 }
 
 rcpparma_bothproducts <- function(x) {
-    .Call(`_IsoDeconvMM_rcpparma_bothproducts`, x)
+    .Call('_IsoDeconvMM_rcpparma_bothproducts', PACKAGE = 'IsoDeconvMM', x)
 }
 
